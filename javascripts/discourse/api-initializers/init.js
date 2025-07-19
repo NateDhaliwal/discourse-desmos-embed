0e9e0d2cf545d830182b012644d56052266c8b5d
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-07-19 18:15:33 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -1,4 +1,5 @@
 import { apiInitializer } from "discourse/lib/api";
+import loadScript from "discourse/lib/load-script";
 
 export default apiInitializer((api) => {
   api.onToolBarCreate(toolbar => {
@@ -8,6 +9,22 @@ export default apiInitializer((api) => {
       icon: 'chart-line',
       title: 'pop_format.title',
       perform: (e) => e.applySurround('[wrap="desmos-graph"]\n', '\n[/wrap]', 'add_desmos_graph')
+    });
+  });
+
+  api.decorateCookedElement((element, helper) => {
+    let apiKey = settings.desmos_api_key;
+    let graphParent = element.querySelector('[data-wrap="desmos-graph"]');
+    let graphEq = graph.textContent;
+    loadScript(`https://www.desmos.com/api/v1.11/calculator.js?apiKey=${apiKey}`).then(() => {
+      let graphEmbed = element.createElement("div");
+      graphEmbed.id = "graph";
+      graphParent.appendChild(graphEmbed);
+      let calculator = Desmos.GraphingCalculator(graphEmbed);
+      calculator.setExpression({
+           id: 'graph1',
+           latex: graphEq.toString()
+       });
     });
   });
 });
