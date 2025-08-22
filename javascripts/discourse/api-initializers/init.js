09b74ee6efd52a9a2aaaf226a14d825a96be8649
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-22 08:36:03 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -3,14 +3,16 @@ import loadScript from "discourse/lib/load-script";
 import I18n from "I18n";
 
 export default apiInitializer((api) => {
-  //I18n.translations["en"].js.composer.add_desmos_graph_equation = "Add equation";
+  const currentLocale = I18n.currentLocale();
+  I18n.translations[currentLocale].js.composer.desmos_graph_equation_placeholder = settings.desmos_graph_equation_placeholder;
+  
   api.onToolbarCreate(toolbar => {
     toolbar.addButton({
       id: 'add-desmos',
       group: 'extras',
       icon: 'chart-line',
       title: themePrefix('desmos.desmos_button_title'),
-      perform: e => e.applySurround('[wrap="desmos-graph"]\n', '\n[/wrap]', 'add_desmos_graph_equation') // add_desmos_graph_euqation is a locale string: edit the text in locales/en.yml.
+      perform: e => e.applySurround('[wrap="desmos-graph"]\n', '\n[/wrap]', 'desmos_graph_equation_placeholder') // add_desmos_graph_euqation is a locale string: edit the text in locales/en.yml.
     });
   });
 
