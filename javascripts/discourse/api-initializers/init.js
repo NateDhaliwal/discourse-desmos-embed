d30eb816685fb7c144c84f96530870e07767b1fe
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-25 19:42:20 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -54,7 +54,7 @@ export default apiInitializer((api) => {
             console.log("Waited 5 seconds!");
           }, 5000); // 5000 milliseconds = 5 seconds
           const data = [trace1];
-          Plotly.newPlot('graph', data);
+          Plotly.newPlot(`graph-${graphParentAll.indexOf(graphParent)}`, data);
           console.log("Plotted");
         } catch (err) {
           console.error(err);
