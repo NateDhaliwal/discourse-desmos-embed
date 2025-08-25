34fd8dcdcd1de0a7949a3593de9689bf06ceef42
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-25 19:36:39 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -15,51 +15,49 @@ export default apiInitializer((api) => {
       perform: e => e.applySurround('[wrap="graph-embed"]\n', '\n[/wrap]', 'graph_equation_placeholder') // add_desmos_graph_euqation is a locale string: edit the text in locales/en.yml.
     });
   });
+  
   api.decorateCookedElement((element, helper) => {
-      let graphParentAll = element.querySelectorAll('div[data-wrap="graph-embed"]');
-      console.log(graphParentAll);
-      if (graphParentAll !== null) {
-        // loadScript("https://unpkg.com/function-plot/dist/function-plot.js");
-        // loadScript("https://cdnjs.cloudflare.com/ajax/libs/function-plot/1.25.1/function-plot.js");
-        let graphParentArray = [...graphParentAll];
-        console.log(graphParentArray);
-        //graphParentArray.forEach(graphParent => {
-          let graphEq = graphParent.textContent;
-          console.log(typeof graphEq);
-          let graphEmbed = document.createElement("div");
-          graphEmbed.id = "graph";
-          graphParent.appendChild(graphEmbed);
-          
-          try {
-            // compile the expression once
-            const expression = graphEq;
-            const expr = math.compile(expression);
+    let graphParentAll = element.querySelectorAll('div[data-wrap="graph-embed"]');
+    console.log(graphParentAll);
+    if (graphParentAll !== null) {
+      // loadScript("https://unpkg.com/function-plot/dist/function-plot.js");
+      // loadScript("https://cdnjs.cloudflare.com/ajax/libs/function-plot/1.25.1/function-plot.js");
+      let graphParentArray = [...graphParentAll];
+      console.log(graphParentArray);
+      //graphParentArray.forEach(graphParent => {
+      let graphEq = graphParent.textContent;
+      console.log(typeof graphEq);
+      let graphEmbed = document.createElement("div");
+      graphEmbed.id = "graph";
+      graphParent.appendChild(graphEmbed);
       
-            // evaluate the expression repeatedly for different values of x
-            const xValues = math.range(-10, 10, 0.5).toArray();
-            const yValues = xValues.map(function (x) {
-              return expr.evaluate({x: x});
-            });
-            console.log(xValues);
-            console.log(yValues);
-            // render the plot using plotly
-            const trace1 = {
-              x: xValues,
-              y: yValues,
-              type: 'scatter'
-            };
-            setTimeout(() => {
-              console.log("Waited 5 seconds!");
-            }, 5000); // 5000 milliseconds = 5 seconds
-            const data = [trace1];
-            Plotly.newPlot('graph', data);
-          }
-          catch (err) {
-            console.error(err);
-          }
-          
-        //});
+      try {
+        // compile the expression once
+        const expression = graphEq;
+        const expr = math.compile(expression);
+  
+        // evaluate the expression repeatedly for different values of x
+        const xValues = math.range(-10, 10, 0.5).toArray();
+        const yValues = xValues.map(function (x) {
+          return expr.evaluate({x: x});
+        });
+        console.log(xValues);
+        console.log(yValues);
+        // render the plot using plotly
+        const trace1 = {
+          x: xValues,
+          y: yValues,
+          type: 'scatter'
+        };
+        setTimeout(() => {
+          console.log("Waited 5 seconds!");
+        }, 5000); // 5000 milliseconds = 5 seconds
+        const data = [trace1];
+        Plotly.newPlot('graph', data);
+      } catch (err) {
+        console.error(err);
       }
+      //});
     }
   });
 });
