274426160b09caa1ba764ee1e46f98080750eb66
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-25 13:17:55 +0000

    Style via Prettier

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -3,26 +3,34 @@ import I18n from "discourse-i18n";
 
 export default apiInitializer((api) => {
   const currentLocale = I18n.currentLocale();
-  I18n.translations[currentLocale].js.composer.graph_equation_placeholder = settings.graph_equation_placeholder;
+  I18n.translations[currentLocale].js.composer.graph_equation_placeholder =
+    settings.graph_equation_placeholder;
 
-  api.onToolbarCreate(toolbar => {
+  api.onToolbarCreate((toolbar) => {
     toolbar.addButton({
-      id: 'add-graph',
-      group: 'extras',
-      icon: 'chart-line',
-      title: themePrefix('graph.graph_button_title'),
-      perform: e => e.applySurround('[wrap="graph-embed"]\n', '\n[/wrap]', 'graph_equation_placeholder') // add_desmos_graph_euqation is a locale string: edit the text in locales/en.yml.
+      id: "add-graph",
+      group: "extras",
+      icon: "chart-line",
+      title: themePrefix("graph.graph_button_title"),
+      perform: (e) =>
+        e.applySurround(
+          '[wrap="graph-embed"]\n',
+          "\n[/wrap]",
+          "graph_equation_placeholder"
+        ), // add_desmos_graph_euqation is a locale string: edit the text in locales/en.yml.
     });
   });
 
   api.decorateCookedElement((element, helper) => {
-    let graphParentAll = element.querySelectorAll('div[data-wrap="graph-embed"]');
+    let graphParentAll = element.querySelectorAll(
+      'div[data-wrap="graph-embed"]'
+    );
 
     if (graphParentAll !== null) {
       let graphParentArray = [...graphParentAll];
 
       // Iterate, in case there are multiple graphs in a single post
-      graphParentArray.forEach(graphParent => {
+      graphParentArray.forEach((graphParent) => {
         // Create new `div` element to show graph in
         let graphEq = graphParent.textContent.trim();
         let graphEmbed = document.createElement("div");
@@ -33,21 +41,21 @@ export default apiInitializer((api) => {
           // Compile the expression
           const expression = graphEq;
           const expr = math.compile(expression);
-  
+
           // Evaluate the expression/equation repeatedly for different values of x
           const xValues = math.range(-10, 10, 0.5).toArray();
           const yValues = xValues.map(function (x) {
-            return expr.evaluate({x: x});
+            return expr.evaluate({ x: x });
           });
-          
+
           // Render the plot using Plotly
           const trace1 = {
             x: xValues,
             y: yValues,
-            type: 'scatter'
+            type: "scatter",
           };
           const data = [trace1];
-          
+
           // Plot the graph
           Plotly.newPlot(graphEmbed, data);
         } catch (err) {}
