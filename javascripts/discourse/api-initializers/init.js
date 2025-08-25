d159605a37b6a9285fe61e467ca5d0e58d3f8da6
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-08-25 20:03:54 +0800

    Update init.js

--- javascripts/discourse/api-initializers/init.js
+++ javascripts/discourse/api-initializers/init.js
@@ -1,11 +1,10 @@
 import { apiInitializer } from "discourse/lib/api";
-import loadScript from "discourse/lib/load-script";
-import I18n from "I18n";
+import I18n from "discourse-i18n";
 
 export default apiInitializer((api) => {
   const currentLocale = I18n.currentLocale();
   I18n.translations[currentLocale].js.composer.graph_equation_placeholder = settings.graph_equation_placeholder;
-  
+
   api.onToolbarCreate(toolbar => {
     toolbar.addButton({
       id: 'add-graph',
@@ -15,38 +14,41 @@ export default apiInitializer((api) => {
       perform: e => e.applySurround('[wrap="graph-embed"]\n', '\n[/wrap]', 'graph_equation_placeholder') // add_desmos_graph_euqation is a locale string: edit the text in locales/en.yml.
     });
   });
-  
+
   api.decorateCookedElement((element, helper) => {
     let graphParentAll = element.querySelectorAll('div[data-wrap="graph-embed"]');
 
     if (graphParentAll !== null) {
-      // loadScript("https://unpkg.com/function-plot/dist/function-plot.js");
-      // loadScript("https://cdnjs.cloudflare.com/ajax/libs/function-plot/1.25.1/function-plot.js");
       let graphParentArray = [...graphParentAll];
 
+      // Iterate, in case there are multiple graphs in a single post
       graphParentArray.forEach(graphParent => {
+        // Create new `div` element to show graph in
         let graphEq = graphParent.textContent.trim();
         let graphEmbed = document.createElement("div");
         graphEmbed.id = `graph-${graphParentArray.indexOf(graphParent)}`;
         graphParent.appendChild(graphEmbed);
-        
+
         try {
-          // compile the expression once
+          // Compile the expression
           const expression = graphEq;
           const expr = math.compile(expression);
-    
-          // evaluate the expression repeatedly for different values of x
+  
+          // Evaluate the expression/equation repeatedly for different values of x
           const xValues = math.range(-10, 10, 0.5).toArray();
           const yValues = xValues.map(function (x) {
             return expr.evaluate({x: x});
           });
-          // render the plot using plotly
+          
+          // Render the plot using Plotly
           const trace1 = {
             x: xValues,
             y: yValues,
             type: 'scatter'
           };
           const data = [trace1];
+          
+          // Plot the graph
           Plotly.newPlot(graphEmbed, data);
         } catch (err) {}
       });
